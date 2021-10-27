94cd071aa155cbc0577fdf9147dcfb10b59f5ba8
Alec Hubacher <qwertyabck@gmail.com>
2021-10-27 17:32:44 -0500

    backend connected to frontend

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
+
+const StudentProfiles = () => {
+  const [studentProfiles, setStudentProfiles] = useState([]);
+  const fetchStudentProfiles = () => {
+    axios.get("http://localhost:8080/api/student/allstudents").then(res => {
+      console.log(res);
+        setStudentProfiles(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchStudentProfiles();
+  }, []);
+
+  return studentProfiles.map((studentProfile, index) => {
+
+    return (
+        <div key={index}>
+          <h1>{studentProfile.id}</h1>
+          <h1>{studentProfile.name}</h1>
+          <h1>{studentProfile.email}</h1>
+          <h1>{studentProfile.dob}</h1>
+          <h1>{studentProfile.age}</h1>
+        </div>
+    )
+  })
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <StudentProfiles />
     </div>
   );
 }
