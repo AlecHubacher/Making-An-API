f0c81eec807b6ac0d1361cba2bc084100ad5ce84
Alec Hubacher <qwertyabck@gmail.com>
2021-10-30 15:07:27 -0500

    testing different json color schemes

--- src/main/frontend/src/APIDocumentation.js
+++ src/main/frontend/src/APIDocumentation.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import Collapsible from "react-collapsible";
+import './APImethodboxes.css'
 export default function APIDoc(){
     return(
         <div className="apidoc">
@@ -15,6 +14,36 @@ export default function APIDoc(){
                         fghfgh fhfghfghfdfd gdfg dfg dfg df  df gdfg dfdfggdfg dfg dfgdfgdfg
                         fhjhfghj This endpoint returns every student in table Student sdf sdf asdfas fasf asf sadfa d
                         fghfgh fhfghfghfdfd gdfg dfg dfg df  df gdfg dfdfggdfg dfg dfgdfgdfg
+                    </p>
+                </div>
+            </div>
+
+            <div className="boxgetendpoint2">
+                <input type="checkbox" id="boxgetendpoint-head2"></input>
+                <label htmlFor="boxgetendpoint-head2" className="getendpoint-label">
+                    <span className="getmethod2">GET</span>
+                    <span className="getendpoint">student/allstudents</span>
+                </label>
+                <div className="collapsible-text-api2">
+                    <p>This endpoint returns every student in table Student sdf sdf asdfas fasf asf sadfa d
+                        fghfgh fhfghfghfdfd gdfg dfg dfg df df gdfg dfdfggdfg dfg dfgdfgdfg
+                        fhjhfghj This endpoint returns every student in table Student sdf sdf asdfas fasf asf sadfa d
+                        fghfgh fhfghfghfdfd gdfg dfg dfg df df gdfg dfdfggdfg dfg dfgdfgdfg
+                    </p>
+                </div>
+            </div>
+
+            <div className="boxgetendpoint3">
+                <input type="checkbox" id="boxgetendpoint-head3"></input>
+                <label htmlFor="boxgetendpoint-head3" className="getendpoint-label">
+                    <span className="getmethod3">GET</span>
+                    <span className="getendpoint">student/allstudents</span>
+                </label>
+                <div className="collapsible-text-api3">
+                    <p>This endpoint returns every student in table Student sdf sdf asdfas fasf asf sadfa d
+                        fghfgh fhfghfghfdfd gdfg dfg dfg df df gdfg dfdfggdfg dfg dfgdfgdfg
+                        fhjhfghj This endpoint returns every student in table Student sdf sdf asdfas fasf asf sadfa d
+                        fghfgh fhfghfghfdfd gdfg dfg dfg df df gdfg dfdfggdfg dfg dfgdfgdfg
                     </p>
                 </div>
             </div>
